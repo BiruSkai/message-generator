b1c04da8730cc35694b615be1d91ec28fe76cdda
Nikolaus Albert <115626969+BiruSkai@users.noreply.github.com>
2023-02-04 18:37:12 +0700

    1st modified

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function addedMsg() {
                 return "You could meet a wonderful person.";
         }
         else if (number == 1) {
-                return "You could get a present.";
+                return "You could get a ......";
         }
         else if (number == 2) {
                 return "You might find an ancient gold treasure";
